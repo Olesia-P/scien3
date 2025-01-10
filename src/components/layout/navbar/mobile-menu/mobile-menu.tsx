444140136cae1c182c7b8e69bd80a536d7eb73c9
Olesia-P <120183444+Olesia-P@users.noreply.github.com>
2025-01-10 18:56:48 +0200

    Added services list to mobile menu

--- src/components/layout/navbar/mobile-menu/mobile-menu.tsx
+++ src/components/layout/navbar/mobile-menu/mobile-menu.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { IoChevronDown } from 'react-icons/io5';
 import cx from 'classnames';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
@@ -8,26 +9,74 @@ import { textNavbar } from '@/utils/texts/layout/text-navbar';
 
 export default function MobileMenu() {
   const language = 'ua';
-
-  const { navlinks } = textNavbar[language];
-
+  const { navlinks, dropdownLinks } = textNavbar[language];
   const router = useRouter();
+
+  const [isServicesBlockOpen, setIsServicesBlockOpen] = useState(false);
+
+  const handleServicesClick = (link: string) => {
+    if (!isServicesBlockOpen) {
+      setIsServicesBlockOpen(true);
+    }
+    if (isServicesBlockOpen) {
+      router.push(link);
+      setIsServicesBlockOpen(false);
+    }
+  };
+
+  useEffect(() => {
+    setIsServicesBlockOpen(false);
+  }, [router.pathname]);
+
   return (
     <div className={css.shadowWrapper}>
       <div className={css.container}>
-        {navlinks.map((navLink) => (
-          <Link
-            className={cx(
-              css.navlink,
-              router.pathname === navLink.link && css.selected,
-            )}
-            href={navLink.link}
-            key={navLink.name}
-            aria-current={router.pathname === navLink.link ? 'page' : undefined}
-          >
-            {navLink.name}
-          </Link>
-        ))}
+        {navlinks.map((navlink) =>
+          navlink.hasDropdown ? (
+            <div key={navlink.name}>
+              <p
+                className={cx(
+                  css.navlink,
+                  isServicesBlockOpen && css.withDropdown,
+                )}
+                onClick={() => handleServicesClick(navlink.link)}
+              >
+                {navlink.name}{' '}
+                <IoChevronDown
+                  className={cx(css.chevron, isServicesBlockOpen && css.turned)}
+                />
+              </p>
+
+              <div
+                className={cx(
+                  css.dropdownBlockWrap,
+                  isServicesBlockOpen && css.opened,
+                )}
+              >
+                <ul className={css.dropdownBlock}>
+                  {dropdownLinks.map((dropdownLink) => (
+                    <Link
+                      href={dropdownLink.link}
+                      key={dropdownLink.link}
+                      className={css.dropdownLink}
+                    >
+                      {dropdownLink.name}
+                    </Link>
+                  ))}
+                </ul>
+              </div>
+            </div>
+          ) : (
+            <Link
+              className={css.navlink}
+              href={navlink.link}
+              key={navlink.name}
+            >
+              {navlink.name}
+            </Link>
+          ),
+        )}
+
         <div className={css.switch}>
           <LangSwitch fontSize="m" />
         </div>
