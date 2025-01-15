ed26ff0390bd61370aa737263cb0daaedaed27b5
Olesia-P <120183444+Olesia-P@users.noreply.github.com>
2025-01-15 18:58:07 +0200

    Responsive: home intro, tablet dragon version, changed general structure

--- src/components/home-components/section-intro/section-intro.tsx
+++ src/components/home-components/section-intro/section-intro.tsx
@@ -3,6 +3,7 @@ import cx from 'classnames';
 import css from './section-intro.module.scss';
 import useIntersectionObserver from '@/hooks/useIntersectionObserver';
 import { textSectionIntro } from '@/utils/texts/home/text-section-intro';
+// import useMediaQuery from '@/hooks/useMediaQuery';
 
 export default function SectionIntro() {
   const language = 'ua';
@@ -19,6 +20,8 @@ export default function SectionIntro() {
     0,
   );
 
+  // const isLargeScreen = useMediaQuery(1025);
+
   return (
     <section className={css.container} ref={introSectionRef}>
       <hgroup className={css.mainHeader}>
@@ -27,19 +30,61 @@ export default function SectionIntro() {
           <p>{caption}</p>
         </div>
       </hgroup>
-      <div className={css.listWrap}>
-        <ul className={cx(css.introList, isObserved && css.animated)}>
-          {ourProducts.map((product) => (
-            <li key={product}>{product}</li>
-          ))}
-        </ul>
+
+      <div className={css.contentWrap}>
+        <div className={css.listWrap}>
+          <ul className={cx(css.list, isObserved && css.animated)}>
+            {ourProducts.map((product) => (
+              <li key={product}>{product}</li>
+            ))}
+          </ul>
+        </div>
+
+        <img
+          src="intro/intro-scene.jpg"
+          alt={illustrationAlt}
+          aria-hidden="true"
+          className={css.illustrationLarge}
+        />
+
+        <img
+          src="intro/kuykuyjrt.png"
+          alt={illustrationAlt}
+          aria-hidden="true"
+          className={css.illustrationSmall}
+        />
       </div>
-      <img
-        src="intro/intro-scene.jpg"
-        alt={illustrationAlt}
-        aria-hidden="true"
-        className={css.illustration}
-      />
+
+      {/* <div className={css.mobileComponent}></div> */}
+      {/* <IntroMobile /> */}
+      {/* <div className={css.mobileContainer}>
+        {' '}
+        <hgroup className={css.mainHeader}>
+          <div className={css.headerWrapper}>
+            <h1>Scien3 Cast Creations</h1>
+            <p>{caption}</p>
+          </div>
+        </hgroup>
+        <div className={css.listWrap}>
+          <ul className={cx(css.introList, isObserved && css.animated)}>
+            {ourProducts.map((product) => (
+              <li key={product}>{product}</li>
+            ))}
+          </ul>
+        </div>
+        <img
+          src="intro/intro-scene.jpg"
+          alt={illustrationAlt}
+          aria-hidden="true"
+          className={css.illustrationLarge}
+        />
+        <img
+          src="intro/kuykuyjrt.png"
+          alt={illustrationAlt}
+          aria-hidden="true"
+          className={css.illustrationSmall}
+        />
+      </div> */}
     </section>
   );
 }
