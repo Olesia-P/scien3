184bc748e903099b276ba73234d3e10033a4d35a
Olesia-P <120183444+Olesia-P@users.noreply.github.com>
2025-01-20 16:58:15 +0200

    Responsive: home, description

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,15 +3,15 @@ import css from '../styles/page-styles/home.module.scss';
 import SectionIntro from '@/components/home-components/section-intro/section-intro';
 // import SectionMarketing from '@/components/home-components/section-marketing/section-marketing';
 // import SectionContacts from '@/components/home-components/section-contacts/section-contacts';
-// import SectionDescription from '@/components/home-components/section-description/section-description';
+import SectionDescription from '@/components/home-components/section-description/section-description';
 // import SectionServices from '@/components/home-components/section-services/section-services';
 
 export default function Home() {
   return (
     <main className={css.container}>
       <SectionIntro />
-      {/* <SectionDescription />
-      <SectionServices />
+      <SectionDescription />
+      {/* <SectionServices />
       <SectionMarketing />
       <SectionContacts /> */}
     </main>
