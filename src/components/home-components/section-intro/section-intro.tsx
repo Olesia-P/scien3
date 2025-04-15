ca289f10bd8967575b340ca109d6bec46b577da2
Olesia-P <120183444+Olesia-P@users.noreply.github.com>
2025-04-15 17:16:18 +0300

    Testing intro scene img srcset

--- src/components/home-components/section-intro/section-intro.tsx
+++ src/components/home-components/section-intro/section-intro.tsx
@@ -9,8 +9,11 @@ import { useLanguage } from '@/hooks/use-language';
 export default function SectionIntro() {
   const language = useLanguage();
 
-  const { caption, ourProducts, illustrationLargeAlt, illustrationSmallAlt } =
-    textSectionIntro[language];
+  const {
+    caption,
+    ourProducts,
+    // illustrationLargeAlt, illustrationSmallAlt
+  } = textSectionIntro[language];
 
   const [isObserved, setIsObserved] = useState(false);
 
@@ -19,6 +22,9 @@ export default function SectionIntro() {
     undefined,
     0,
   );
+
+  // eslint-disable-next-line no-console
+  console.log(window.devicePixelRatio);
 
   return (
     <div className={css.container} ref={introSectionRef}>
@@ -34,18 +40,46 @@ export default function SectionIntro() {
         </div>
 
         {/* for desktop > 1024px */}
-        <img
+        {/* <img
           src="intro/intro-scene.jpg"
           alt={illustrationLargeAlt}
           className={css.illustrationLarge}
-        />
+          // loading="lazy"
+        /> */}
+
+        <picture>
+          <source
+            media="(max-width: 1024px)"
+            srcSet="
+            /intro/dragon_400.webp 400w,
+            /intro/dragon_795.webp 795w,
+            /intro/dragon_795.png 795w"
+          />
+          <source
+            media="(min-width: 1024px)"
+            srcSet="
+            /intro/intro_scene_1400.webp 1400w,
+            /intro/intro_scene_1400.jpg 1400w,
+            /intro/intro_scene_1920.webp 1920w,
+            /intro/intro_scene_1920.jpg 1920w,
+             /intro/intro_scene_2200.webp 2200w,
+             /intro/intro_scene_2200.jpg 2200w"
+            // sizes="100vw"
+          />
+          <img
+            src="/intro/intro_scene_1400.jpg"
+            alt=""
+            className={css.illustration}
+            sizes="100vw"
+          />
+        </picture>
 
         {/* for tablet <= 1024px */}
-        <img
+        {/* <img
           src="intro/dragon.png"
           alt={illustrationSmallAlt}
           className={cx(css.illustrationSmall, isObserved && css.animated)}
-        />
+        /> */}
       </section>
     </div>
   );
